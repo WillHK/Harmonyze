4e136c376d8b88d7e8f358f2106a204c28064f74
Dylan T. Moberg <dylanmoberg@msn.com>
2015-04-20 13:46:37 -0700

    attempting to add ajax requests

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+'use strict'
+// var filepath = function(image) {
+//     image.forEach(function(img) {
+//       filePathArray.push(img.link);
+
+//     });
+//   };
+ $.ajax({
+    url: 'http://api.eventful.com/json/events/search?location=Seattle&category=music&date=future&app_key=tVX3L4p7bMfxKVRq',
+    method: 'GET',
+ headers: {
+      // 'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With'
+    }
+  })
+  .done(function(res) {
+    concertObject = res.events.event[1]
+    console.log(concertObject);
+
+  })
+  .fail(function(err) {
+    console.log(err);
+  });
+
+// });
