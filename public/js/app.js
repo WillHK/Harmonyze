297b20a78cb6b69c3a370ba221eed19fb0efb30f
Dylan T. Moberg <dylanmoberg@msn.com>
2015-04-20 14:39:40 -0700

    added functioning ajax request

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,15 @@
 'use strict'
-// var filepath = function(image) {
-//     image.forEach(function(img) {
-//       filePathArray.push(img.link);
 
-//     });
-//   };
  $.ajax({
-    url: 'https://api.eventful.com/json/events/search?location=Seattle&category=music&date=future&app_key=tVX3L4p7bMfxKVRq',
+    url: 'http://api.jambase.com/events?zipCode=98101&radius=50&page=0&api_key=mnenyzm7ngsrdnmmpqpqq6yd',
     method: 'GET',
- headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'X-Requested-With'
-    }
-  })
+   })
   .done(function(res) {
-    concertObject = res.events.event[1]
-    console.log(concertObject);
+    console.log(res);
 
   })
   .fail(function(err) {
     console.log(err);
   });
 
-// });
+
