5a2f93d6d4b40a14dba6142f53fa7f335f26cb70
WillHK <will.haeck@gmail.com>
2015-04-21 14:00:32 -0700

    work on speeding up API calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 'use strict'
 $(function() {
+  var idCounter = 0;
   var pageCounter = 0;
+  var tempEventList = 0;
   var User = function(userData) {
     this.userId = userData.id;
     this.userName = userData.name;
@@ -25,23 +27,25 @@ $(function() {
     return articleEl;
   }
   var Event = function(eventData) {
+    this.title = eventData.title;
     this.place = eventData.place;
     this.date = eventData.date;
     this.artistName = eventData.artistName;
     this.artistImage = eventData.artistImage;
-
+    this.eventDescription = eventData.description;
+    this.latitude = eventData.latitude;
+    this.longitude = eventData.longitude;
   };
   var eventList = [];
   var getNewEvents = function() {
-    pageCounter++;
-    var tempEventList = [];
+
     console.log('getNewEvents called');
     $.ajax({
 
       method: 'GET',
 
       // The URL to make the request to.
-      url: 'http://api.eventful.com/json/events/search?location=Seattle&category=music&date=Future&image_sizes=medium&sort_order=date&sort_direction=ascending&page_number=' + pageCounter + '&page_size=20&app_key=tVX3L4p7bMfxKVRq',
+      url: 'http://api.eventful.com/json/events/search?location=Seattle&category=music&date=Future&image_sizes=medium&sort_order=date&sort_direction=ascending&page_number=' + pageCounter + '&page_size=10&app_key=tVX3L4p7bMfxKVRq',
 
 
       contentType: 'text/plain',
@@ -66,11 +70,18 @@ $(function() {
         console.log(res.events);
         res.events.event.forEach(function(event){
           if(event.image != null){
-            if(event.start_time > "2015-04-19")
+            if(event.start_time > "2015-04-19") {
+              if (tempEventList.length < 4) {
               eventList.push(new Event({"artistName": event.title, "place": event.venue_name, "date": event.start_time, "artistImage": event.image.medium || 1}));
               tempEventList.push(new Event({"artistName": event.title, "place": event.venue_name, "date": event.start_time, "artistImage": event.image.medium || 1}));
+              }
+            }
           }
         });
+        if (tempEventList.length < 4) {
+          pageCounter++;
+          getNewEvents();
+        }
         console.log(eventList);
         tempEventList.forEach(function(event){
           $('.container').append(render(event));
@@ -81,6 +92,8 @@ $(function() {
         console.log("error: " + err + " " + res);
       }
     });
+  pageCounter++;
+  tempEventList = [];
   };
   $('#moreShows').click(getNewEvents);
   getNewEvents();
