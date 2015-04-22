a501fb1fffbf39d0a3aa9f9cdc7cbdd43e22bb37
Dylan T. Moberg <dylanmoberg@msn.com>
2015-04-22 16:52:18 -0700

    profile page is now fully functional using form input

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,6 +6,16 @@ var newUser = JSON.parse(newUserString);
 
 $('#userImg').append('<img src="' + newUser.picture + '"/>');
 $('#userImg').append('<p>' + newUser.userId + '</p>');
+$('#bio').append('<p>' + newUser.bio + '</p>');
+var interests = newUser.orientation;
+var userInterests = interests.replace("_", " ");
+$('#interested_in').append('<p>' + userInterests + '</p>');
+console.log(newUser.genres[0].value)
+for (var i = 0; i < newUser.genres.length; i++) {
+  var genres = newUser.genres[i].value;
+  genres.replace('_', " ");
+  $('#genres').append('<li>' + genres + '</li>');
+}
 
 // console.log(newUser.picture)
 
